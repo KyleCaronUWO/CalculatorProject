200c70c1a4859f4c2f397c3e0b9685963079537c
KyleCaronUWO <kcaron6@uwo.ca>
2026-05-10 19:57:51 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+## Calculator Mini project
+## Created by Kyle Caron
+
+## Collect Data from users
+
+first_number = input('Please enter the first number:')
+first_number = int(first_number)
+second_number = input('Please enter the second number:')
+second_number = int(second_number)
+
+## Declaring operation variables
+
+add = (first_number + second_number)
+subtract = (first_number - second_number)
+multiply = (first_number * second_number)
+divide = (first_number / second_number)
+
+## Printing results
+
+print('The totals are:')
+print('Addition:', add)
+print('Subtraction:', subtract)
+print('Multiplication:', multiply)
+print('Division:', divide)
